13b3eda8c5807cce74fc8ec8b7f9543c36200ed8
Janko <janko.prester@gmail.com>
2020-04-18 17:20:07 +0200

    update packages

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,21 +1,17 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { applyMiddleware, createStore } from 'redux';
-import thunkMiddleware from 'redux-thunk';
+import React from "react";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import thunkMiddleware from "redux-thunk";
 
-import './index.css';
-import reducers from './reducers/index';
+import "./index.css";
+import reducers from "./reducers/index";
 
-export default ({ children = {}, initialState = {}}) => {
+export default ({ children, initialState }: any) => {
   const store = createStore(
     reducers,
     initialState,
-    applyMiddleware(thunkMiddleware),
+    applyMiddleware(thunkMiddleware)
   );
 
-  return (
-    <Provider store={store}>
-      {children}
-    </Provider>
-  );
+  return <Provider store={store}>{children}</Provider>;
 };
