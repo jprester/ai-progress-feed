24b5a5dff6bf8da81c67648790cfda5870d322de
Janko <janko.prester@gmail.com>
2020-07-29 19:12:59 +0200

    small fixes

--- src/components/pages/AboutPage.tsx
+++ src/components/pages/AboutPage.tsx
@@ -3,10 +3,7 @@ import React from "react";
 const About = () => (
   <div className="about-page">
     <h3 className="feed-list-title">About</h3>
-    <p>
-      This app aggregates the news from various media sources. You can list the
-      news for specific category or make a search.
-    </p>
+    <p>This app aggregates the news about AI from various RSS sources.</p>
   </div>
 );
 
