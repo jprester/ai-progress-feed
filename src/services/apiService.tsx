170e57c4f620d7a25a3996f928ec1afb95adbb46
Janko <janko.prester@gmail.com>
2020-07-30 17:41:32 +0200

    small refactor

--- src/services/apiService.tsx
+++ src/services/apiService.tsx
@@ -1,9 +1,9 @@
 import axios from "axios";
 import Parser from "rss-parser";
+
+import { PROXY_SERVER } from "../helpers/apiConfig";
 let parser = new Parser();
 
 export async function getArticles(source: string) {
-  return parser.parseURL(
-    `${"https://cors-proxy-janko.herokuapp.com/"}${source}`
-  );
+  return parser.parseURL(`${PROXY_SERVER}/${source}`);
 }
